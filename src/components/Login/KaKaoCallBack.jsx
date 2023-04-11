21dd00c9f07386edb8a86a9a892dc47796e51a00
KIJUNG-CHAE <daiywlf5251@gmail.com>
2023-04-11 20:39:37 +0900

    Chore: cookie check

--- src/components/Login/KaKaoCallBack.jsx
+++ src/components/Login/KaKaoCallBack.jsx
@@ -24,8 +24,8 @@ const KaKaoCallBack = () => {
   }));
 
   useEffect(() => {
-    // const params = new URL(document.location.toString()).searchParams;
-    // const code = params.get('code');
+    const params = new URL(document.location.toString()).searchParams;
+    const code = params.get('code');
     // const redirectUri = process.env.REACT_APP_KAKAO_LOGIN_REDIRECT_URI;
     // const GRANT_TYPE = 'authorization_code';
     // const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
@@ -34,48 +34,49 @@ const KaKaoCallBack = () => {
      * redirect_url: 인가코드 검증용 프론트엔드에서 사용한 redirect_uri
      * code: 카카오 서버로 부터 받은 인가코드
      */
-    // axios
-    //   .get(
-    //     `http://ec2-52-79-40-57.ap-northeast-2.compute.amazonaws.com/api/v1/auth/login?code=${CODE}&redirect_url=${REDIRECT_URI}`,
-    //     {}
-    //   )
-    //   .then((res) => {
-    //     console.log(res);
-    //     const { data } = res;
-    //     const { access_token } = data;
-    //     if (access_token) {
-    //     }
-    //   });
 
-    const code = 'n24ehneb7f5j9gi';
+    console.log(code);
+    axios
+      .get(`https://api.dayone.kr/api/v1/auth/login?code=${code}`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res);
+        // const { data } = res;
+        // const { access_token } = data;
+        // if (access_token) {
+        // }
+      });
 
-    dispatch(login({ code }));
+    // const code = 'n24ehneb7f5j9gi';
+
+    // dispatch(login({ code }));
   }, [dispatch]);
 
-  useEffect(() => {
-    if (authError) {
-      console.log('오류발생');
-      console.log(authError);
-      alert('인증 오류! 다시 로그인 해주세요.');
-      navigate('/login');
-      return;
-    }
-    if (auth) {
-      console.log('로그인 성공');
-      dispatch(check());
-    }
-  }, [auth, authError, dispatch]);
+  // useEffect(() => {
+  //   if (authError) {
+  //     console.log('오류발생');
+  //     console.log(authError);
+  //     alert('인증 오류! 다시 로그인 해주세요.');
+  //     navigate('/login');
+  //     return;
+  //   }
+  //   if (auth) {
+  //     console.log('로그인 성공');
+  //     dispatch(check());
+  //   }
+  // }, [auth, authError, dispatch]);
 
-  useEffect(() => {
-    if (user) {
-      navigate('/');
-      try {
-        localStorage.setItem('user', JSON.stringify(user));
-      } catch (e) {
-        console.log('로컬 스토리지가 작동하지 않습니다.');
-      }
-    }
-  }, [navigate, user]);
+  // useEffect(() => {
+  //   if (user) {
+  //     navigate('/');
+  //     try {
+  //       localStorage.setItem('user', JSON.stringify(user));
+  //     } catch (e) {
+  //       console.log('로컬 스토리지가 작동하지 않습니다.');
+  //     }
+  //   }
+  // }, [navigate, user]);
 
   return <></>;
 };
